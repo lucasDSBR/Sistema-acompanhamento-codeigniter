09376831af3cc5d8614d6051ada6a87bad9a2e0d
lucas <lucasmaciel6690@gmail.com>
2022-02-11 23:13:09 -0300

    Colocando primeiros arquivos

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,9 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-
+$routes->get('/dashboard', 'Home::dashboard');
+$routes->post('/validLogin', 'Login::validLogin');
+$routes->get('/uploadResult/(:any)', 'Upload::uploadPage');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
